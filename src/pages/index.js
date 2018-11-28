9f77056b399a8e429e2051dff7c3e75d24bd44a9
tkallio <teppo@kallio.me>
2018-11-28 10:39:23 +0200

    Added splash page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
+import "../styles/main.sass"
+
 export default class IndexPage extends React.Component {
   render() {
 
     return (
-      <div>
-        Coming soon
+      <div className="full-page">
+        <div className="full-page-content">
+          <img src="/img/logo-white.svg" alt="Teppo Kallio"/>
+        </div>
       </div>
     )
   }
---
+++ src/templates/blog-post.js
@@ -0,0 +1,13 @@
+import React, { Component, PureComponent, Fragment } from 'react'
+
+
+export function BlogPostTemplate(props) {
+  
+  console.log(props)
+
+  return (
+    <div>
+      Show blogpost
+    </div>
+    )
+}
